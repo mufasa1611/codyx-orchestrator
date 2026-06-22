d38f5338da4f505a348eae8fca47d1a1111e58e8
mufasa1611 <mufasa1611@gmail.com>
2026-06-22 18:25:36 +0200

    Update core temp path test for codyx

--- packages/core/test/global.test.ts
+++ packages/core/test/global.test.ts
@@ -6,7 +6,7 @@ import { Global } from "@cody/core/global"
 
 describe("global paths", () => {
   test("tmp path is under the system temp directory", () => {
-    expect(Global.Path.tmp).toBe(path.join(os.tmpdir(), "cody"))
+    expect(Global.Path.tmp).toBe(path.join(os.tmpdir(), "codyx"))
     expect(Global.make().tmp).toBe(Global.Path.tmp)
   })
 
