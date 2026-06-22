ee70c3a0615f93dcf572dd7b5b6f156c3f937178
mufasa1611 <mufasa1611@gmail.com>
2026-06-22 17:52:19 +0200

    Target generated output formatting

--- script/generate.ts
+++ script/generate.ts
@@ -6,4 +6,4 @@ await $`bun ./packages/sdk/js/script/build.ts`
 
 await $`bun dev generate > ../sdk/openapi.json`.cwd("packages/codyx")
 
-await $`bun ./script/format.ts`
+await $`bun prettier --write packages/sdk/openapi.json packages/sdk/js/src/gen packages/sdk/js/src/v2`
